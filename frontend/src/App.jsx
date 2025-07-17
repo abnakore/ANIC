3d7705c31512b37b95372f8f59086da654e04237
abnakore <abdullahinakore@gmail.com>
2025-07-17 09:06:12 +0100

    Setup Tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,12 @@ function App() {
   return (
     <>
       <h1 class="text-3xl font-bold underline"> ANIS </h1>
+
+      <button className="bg-green-islamic text-cream hover:bg-green-dark bg-primary">
+        Donate Now
+      </button>
+
+      <div className="border-2 border-gold-light p-4">...</div>
     </>
   );
 }
